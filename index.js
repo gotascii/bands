274af479aaed107c63121ae99cf4bd04bfd70510
Justin Marney <gotascii@gmail.com>
2020-12-16 10:36:32 -0800

    eslint, de-const, move arg parsing to top-level

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
+var argParser = require('minimist');
 var bands = require('./lib/bands');
-bands.generateCsv();
+
+var argv = argParser(process.argv.slice(2));
+var bandcampUrl = argv.l;
+
+bands.generateCsv(bandcampUrl);
