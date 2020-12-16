be1f43789ed45f902a92e84931812a819d9a9503
Justin Marney <gotascii@gmail.com>
2020-12-16 09:14:03 -0800

    use lib structure

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 #!/usr/bin/env node
 
-require('dotenv').config();
-
-const bandcamp = require('./bandcamp');
-const discogs = require('./discogs');
-const logger = require('./logger');
-const metalArchives = require('./metalArchives')
-
-async function log() {
-  info = await bandcamp.findInfo();
-  params = {
-    artist: info.artist,
-    title: info.title
-  };
-  info.label = await metalArchives.findLabel(params);
-  info.label ||= await discogs.findLabel(params);
-  logger.log(info);
-}
-
-log();
+var bands = require('./lib/bands');
+bands.generateCsv();
