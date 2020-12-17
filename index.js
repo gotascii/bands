4711d82d041088202f8906e857440d8375866e94
Justin Marney <gotascii@gmail.com>
2020-12-17 01:08:04 -0800

    initial working site

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 #!/usr/bin/env node
 
 var argParser = require('minimist');
+var argv = argParser(process.argv.slice(2), {
+  alias: { e: 'env', i: 'id', s: 'site' }
+});
+
+var envOpts = {};
+if (argv.env) { envOpts.path = argv.env; }
+require('dotenv').config(envOpts);
+
 var bands = require('./lib/bands');
+var bandcampUrl = argv._[0];
+if (bandcampUrl) {
+  bands.generateCsv(bandcampUrl);
+}
 
-var argv = argParser(process.argv.slice(2));
-var bandcampUrl = argv.l;
-
-bands.generateCsv(bandcampUrl);
+var site = require('./lib/site');
+var siteDirPath = argv.site || '.';
+// remove || after done building
+var spreadsheetId = argv.id || '1bMOAYEKvqNShWG-ukFSkSnA6QZ1jQX-G9uCOpneAIMQ';
+if (spreadsheetId) {
+  site.generate(siteDirPath, spreadsheetId);
+}
