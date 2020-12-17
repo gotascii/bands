9210a95ceec98800caf20ea12dba5c7bf50ff99e
Justin Marney <gotascii@gmail.com>
2020-12-17 14:59:54 -0800

    refactoring site

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var argv = argParser(process.argv.slice(2), {
 });
 
 var envOpts = {};
-if (argv.env) { envOpts.path = argv.env; }
+if (argv.env) { envOpts.path = `${argv.env}/.env`; }
 require('dotenv').config(envOpts);
 
 var bands = require('./lib/bands');
@@ -17,8 +17,7 @@ if (bandcampUrl) {
 
 var site = require('./lib/site');
 var siteDirPath = argv.site || '.';
-// remove || after done building
-var spreadsheetId = argv.id || '1bMOAYEKvqNShWG-ukFSkSnA6QZ1jQX-G9uCOpneAIMQ';
+var spreadsheetId = argv.id || process.env.GOOGLE_SSID;
 if (spreadsheetId) {
   site.generate(siteDirPath, spreadsheetId);
 }
